ba2cfd15b02b7e2c479a4f9f15b1d116d55c9c60
Seb Haan <sebhaan@gmail.com>
2023-01-12 18:39:01 +1100

    Add more info

--- python_scripts/__init__.py
+++ python_scripts/__init__.py
@@ -1,1 +1,18 @@
-# This directory includes multiple example scripts for processing, analysing, and generating models for soil science.
+# This directory contains Python scripts for processing, analyzing, and generating models for soil science.
+
+__title__ = "AgReFed-ML: Machine learning tools for modelling and predicting agriculture systems and their uncertainties"
+__description__ = """
+This directory contains Python scripts for processing, analyzing, and generating models for soil science
+as part of the AgReFed project.
+The software was developed by the Sydney Informatics Hub, a core research facility of the University of Sydney, 
+as part of the project Mechanistic and data-driven models under uncertainty for agricultural systems for the 
+Agricultural Research Federation (AgReFed).
+"""
+__uri__ = "https://github.com/Sydney-Informatics-Hub/AgReFed-ML"
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = "Sebastian Haan"
+__license__ = "LGPL-3.0 License"
+
+
+
